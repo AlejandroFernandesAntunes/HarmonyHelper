39a1bd6dc86221249bb1c210b91ade6f4d2ceac3
AlejandroFernandesAntunes <alejandro.fernandes@neonroots.com>
2016-03-13 10:53:36 -0300

    Initial Commit

---
+++ HarmonyHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HarmonyHelper
+//
+//  Created by Ale Fernandes Antunes on 3/13/16.
+//  Copyright © 2016 Ale Fernandes Antunes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
