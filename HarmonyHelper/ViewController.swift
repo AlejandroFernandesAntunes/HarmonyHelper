d961234cc0561a472163771884b667b383d2bbb1
AlejandroFernandesAntunes <alejandro.fernandes@neonroots.com>
2016-03-16 16:06:24 -0300

    Analyze sequence

--- HarmonyHelper/ViewController.swift
+++ HarmonyHelper/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
   let sequence_placeHolder = "Sequence"
+  let analysis_placeHolder = "Analysis"
   // MARK: Properties
   @IBOutlet weak var sequenceLbl: UILabel!
   @IBOutlet weak var analysisLbl: UILabel!
@@ -23,12 +24,117 @@ class ViewController: UIViewController, UITextFieldDelegate {
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
+ 
+ func analyzeSequence() {
+  var harmonized_tones = [String: [String]]()
   
+  let tones = ["C", "C#","D","D#","E", "F", "F#", "G", "G#", "A", "A#", "B"]
+  
+  var coincidences = [String:Int]()
+  coincidences["C"] = 0
+  coincidences["C#"] = 0
+  coincidences["D"] = 0
+  coincidences["D#"] = 0
+  coincidences["E"] = 0
+  coincidences["F"] = 0
+  coincidences["F#"] = 0
+  coincidences["G"] = 0
+  coincidences["A"] = 0
+  coincidences["B"] = 0
+  
+  // harmonizations
+  var c = [String]()
+  c.append("C")
+  c.append("D")
+  c.append("E")
+  c.append("F")
+  c.append("G")
+  c.append("A")
+  c.append("B")
+  harmonized_tones["C"] = c
+  
+  var d = [String]()
+  d.append("C#")
+  d.append("D")
+  d.append("E")
+  d.append("F")
+  d.append("G#")
+  d.append("A")
+  d.append("B")
+  harmonized_tones["D"] = d
+  
+  var e = [String]()
+  e.append("C#")
+  e.append("D#")
+  e.append("E")
+  e.append("F")
+  e.append("G#")
+  e.append("A#")
+  e.append("B")
+  harmonized_tones["E"] = e
+  
+  var f = [String]()
+  f.append("C")
+  f.append("D")
+  f.append("E")
+  f.append("F")
+  f.append("G")
+  f.append("A")
+  f.append("Bb")
+  harmonized_tones["F"] = f
+  
+  var g = [String]()
+  g.append("C#")
+  g.append("D")
+  g.append("E")
+  g.append("F#")
+  g.append("G")
+  g.append("A")
+  g.append("B")
+  harmonized_tones["G"] = g
+  
+  
+  
+  let sequence = sequenceLbl.text
+  let sequenceArr = sequence!.characters.split{$0 == " "}.map(String.init)
+
+  if (sequence != sequence_placeHolder) {
+   for tone:String in tones {
+     let harmonized:[String]? = harmonized_tones[tone]
+       if(harmonized != nil) {
+        for grade:String in harmonized!{
+         if sequenceArr.contains(grade) {
+          coincidences[tone] = coincidences[tone]! + 1
+         }
+        }
+      }
+    }
+  }
+ 
+ 
+   // The one that has the more coincidence will be the tone it's on
+   let maxim = coincidences.values.maxElement()
+   var founded:String = ""
+   if(maxim == 0) {
+    founded = analysis_placeHolder
+   } else {
+    for (key, value) in coincidences {
+     if (value == maxim)
+     {
+      founded =  founded + " " + key
+     }
+    }
+   }
+  analysisLbl.text = founded
+ }
+
+ 
   func addChord(newText: String) {
     if(sequenceLbl.text == sequence_placeHolder) {
       sequenceLbl.text = ""
     }
     sequenceLbl.text =  sequenceLbl.text! + " " + newText
+    analyzeSequence()
   }
     
   func removeLastChord() {
@@ -42,6 +148,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
    
    if(sequenceLbl.text?.characters.count == 0) {
     sequenceLbl.text=sequence_placeHolder
+   } else {
+     analyzeSequence()
    }
   }
     
